8b0530918441e9d607121621756be1efe12fcad6
Chase Struse <chasestruse@gmail.ocm>
2022-04-13 17:34:12 -0500

    Began routing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import Directory from './components/directory/directory.component';
+import Home from './routes/home/home.component';
 
 const App = () => {
   return (
-    <Directory />
+    <Home />
   );
 }
 

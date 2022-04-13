48411dbb2e0e2d522569f2ae707ddd87ab8abd20
Chase Struse <chasestruse@gmail.ocm>
2022-04-13 18:14:15 -0500

    Added navbar styling

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,14 +1,22 @@
-import { Outlet } from 'react-router-dom';
-import './navigation.scss';
-
+import { Fragment } from 'react';
+import { Outlet, Link } from 'react-router-dom';
+import './navigation.style.scss';
+import {ReactComponent as CrwnLogo} from '../../assets/crown.svg'
 const Navigation = () => {
   return(
-    <div>
-      <div>
-        <h1>I am Navbar</h1>
+    <Fragment>
+      <div className='navigation'>
+          <Link className='logo-container' to='/'>
+            <div> <CrwnLogo/> </div>
+          </Link>
+          <div className='nav-links-container'>
+            <Link className='nav-link' to='/shop'>
+                SHOP
+            </Link>
+          </div>
       </div>
       <Outlet />
-    </div>
+    </Fragment>
   );
 };
 
